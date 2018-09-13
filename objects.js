30bcc8f95f932a6afa96abb55e62c30310f160fa
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-13 00:43:41 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {Gorillaz: "Saturnz Barz"};
+
+
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  console.log(Object.assign({}, playlist, {[artistName]: songTitle}));
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+  console.log(playlist);
+}
